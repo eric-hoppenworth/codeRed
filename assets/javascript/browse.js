e7bd84c9395a9bd12459cb14edcfb57b2f4508ec
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-06-28 17:16:18 -0400

    working with populating search results

--- assets/javascript/browse.js
+++ assets/javascript/browse.js
@@ -79,6 +79,37 @@ function showResults(resultArray){
 		holder.append(image).append(paraName).append(paraGenre);
 		appender.append(holder);
 	}
-	$("#preview-coverflow").coverflow("refresh");
+	initializeCoverflow();
 }
 
+function initializeCoverflow(){
+	//For the Carousel on the Browser Page
+    if ($.fn.reflect) {
+        $('#preview-coverflow .cover').reflect();   // only possible in very specific situations
+    }
+
+    $('#preview-coverflow').coverflow({
+        index: 6,
+        density: 2,
+        innerOffset: 50,
+        innerScale: .7,
+        animateStep: function(event, cover, offset, isVisible, isMiddle, sin, cos) {
+            if (isVisible) {
+                if (isMiddle) {
+                    $(cover).css({
+                        'filter': 'none',
+                        '-webkit-filter': 'none'
+                    });
+                } else {
+                    var brightness = 1 + Math.abs(sin),
+                        contrast = 1 - Math.abs(sin),
+                        filter = 'contrast('+contrast+') brightness('+brightness+')';
+                    $(cover).css({
+                        'filter': filter,
+                        '-webkit-filter': filter
+                    });
+                }
+            }
+        }
+    });
+}
