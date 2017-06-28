ec60ff5a5a91668257935d799a13ac9562009168
eric hoppenworth <eric.hoppenworth@gmail.com>
2017-06-27 22:49:16 -0400

    psuedo progress bar

--- assets/javascript/account.js
+++ assets/javascript/account.js
@@ -212,7 +212,7 @@ function storeInServer(user,link, fileType = "audio",objectType = "User"){
 			var endPoint = firebase.storage().ref(objectType+"s/" + user.key + "/profile");
 		}
 		
-		endPoint.put(data.fileBlob).then(function(snapshot){
+		var uploadTask = endPoint.put(data.fileBlob).then(function(snapshot){
 			var fileURL = endPoint.getDownloadURL().then(function(url){
 				//store that shit
 				if (fileType === "audio"){
@@ -230,6 +230,32 @@ function storeInServer(user,link, fileType = "audio",objectType = "User"){
 				
 			});
 		});
+		// Register three observers:
+		// 1. 'state_changed' observer, called any time the state changes
+		// 2. Error observer, called on failure
+		// 3. Completion observer, called on successful completion
+		uploadTask.on('state_changed', function(snapshot){
+		  // Observe state change events such as progress, pause, and resume
+		  // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+		  var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+		  console.log('Upload is ' + progress + '% done');
+		  switch (snapshot.state) {
+		    case firebase.storage.TaskState.PAUSED: // or 'paused'
+		      console.log('Upload is paused');
+		      break;
+		    case firebase.storage.TaskState.RUNNING: // or 'running'
+		      console.log('Upload is running');
+		      break;
+		  }
+		}, function(error) {
+		  // Handle unsuccessful uploads
+		}, function() {
+		  // Handle successful uploads on complete
+		  // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+		  //var downloadURL = uploadTask.snapshot.downloadURL;
+		});
+
+
     }).catch(function(error) {
   		console.error(error);
     });
